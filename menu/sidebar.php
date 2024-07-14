a129ac00ec31594ee6ee83360427015fa8292cf0
Michael Caesario Septiadi <155882662+Michaelcs71@users.noreply.github.com>
2024-07-14 14:48:56 +0700

    yabegitulah

---
+++ menu/sidebar.php
@@ -0,0 +1,38 @@
+<div class="vertical-menu">
+
+                <div data-simplebar class="h-100">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+                        <!-- Left Menu Start -->
+                        <ul class="metismenu list-unstyled" id="side-menu">
+                            <li class="menu-title" data-key="t-menu">Menu</li>
+
+                            <li>
+                                <a href="index.php?link=dashboard">
+                                    <i data-feather="home"></i>
+                                    <span data-key="t-dashboard">Dashboard</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="index.php?link=karyawan">
+                                    <i data-feather="grid"></i>
+                                    <span data-key="t-dashboard">Karyawan</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="index.php?link=laporan">
+                                    <i data-feather="home"></i>
+                                    <span data-key="t-dashboard">Laporan</span>
+                                </a>
+                            </li>
+                            
+                            
+                        </ul>
+
+                    </div>
+                    <!-- Sidebar -->
+                </div>
+            </div>
