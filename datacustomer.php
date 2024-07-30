fe2605015a8610223a2d4cff81235bb154c1fe42
WangiVidiasasti <322110010@student.machung.ac.id>
2024-07-30 11:26:39 +0700

    Perbaiki header desa kucur

--- datacustomer.php
+++ datacustomer.php
@@ -27,7 +27,7 @@ $data = Tampil_Data('karyawan');
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0 font-size-18">Data Desa Kucur</h4>
+                        <h4 class="mb-sm-0 font-size-18">Data Laundry</h4>
 
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
