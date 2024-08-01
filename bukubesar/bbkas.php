a7079cd82c2542deacf8061775662802de12d469
Zxring <322110001@student.machung.ac.id>
2024-08-01 15:37:11 +0700

    mmmmm

--- bukubesar/bbkas.php
+++ bukubesar/bbkas.php
@@ -11,14 +11,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/UasSia/lib/function.php"; // Pastikan
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0 font-size-18">Data Laundry</h4>
+                        <h4 class="mb-sm-0 font-size-18">Buku Besar Kas</h4>
 
-                        <div class="page-title-right">
-                            <ol class="breadcrumb m-0">
-                                <li class="breadcrumb-item"><a href="javascript: void(0);">Data Status</a></li>
-                                <li class="breadcrumb-item active">Data Status</li>
-                            </ol>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -28,7 +22,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/UasSia/lib/function.php"; // Pastikan
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title">Data Status</h4>
+                            <h4 class="card-title">Buku Besar Kas</h4>
                         </div>
                         <div class="card-body">
                             <table id="datatable-buttons" class="table table-bordered dt-responsive nowrap w-100 table-striped table-hover">
