d4c73350cf06ea3d91a8b5c1f6afc39a277d4685
Zxring <322110001@student.machung.ac.id>
2024-08-01 15:52:48 +0700

    jurnal umum

--- jurnalumum.php
+++ jurnalumum.php
@@ -11,7 +11,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/UasSia/lib/function.php"; // Pastikan
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0 font-size-18">Buku Besar Kas</h4>
+                        <h4 class="mb-sm-0 font-size-18">Jurnal Umum</h4>
 
                     </div>
                 </div>
@@ -22,8 +22,12 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/UasSia/lib/function.php"; // Pastikan
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title">Buku Besar Kas</h4>
+                            <h4 class="card-title">Jurnal Umum</h4>
                         </div>
+                        <div class="card-body">
+                            <button id="updateDataBtn" type="button" class="btn btn-primary mb-sm-2" onclick="window.location.href='updatejurnal.php'">Update Data</button>
+
+                            <div style="overflow-x:auto;"></div>
                         <div class="card-body">
                             <table id="datatable-buttons" class="table table-bordered dt-responsive nowrap w-100 table-striped table-hover">
                                 <thead class="table-light">
